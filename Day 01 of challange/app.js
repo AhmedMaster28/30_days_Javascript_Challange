481b11527f4e4c087cb5aea5c323c9eb8a85cd2a
AhmedMaster28 <ahmednsohail0@gmail.com>
2024-07-13 11:03:51 +0500

    Day 01 of 30 days of  javascript challange
    In this challenge, i have learned about the variables of javascript like var, let and const

---
+++ Day 01 of challange/app.js
@@ -0,0 +1,74 @@
+//Day 01 of challange
+// Activity one
+
+// task 01
+
+var number; 
+number=33;
+// console.log(number)
+
+// task 02
+
+let str;
+str="Ahmed";
+// console.log(str)
+
+// Activity 02
+// task 03
+
+const con=true;
+// console.log(con);
+
+// Activity 03
+// task 04
+
+let digit=123;
+
+// console.log("The type of variable digit is ",typeof(digit))
+// console.log("The value of digit is  ",digit)
+
+let word="Chai aur code";
+
+// console.log("The type of variable word is ",typeof(word))
+// console.log("The value of word is  ",word)
+
+let bool=true;
+
+// console.log("The type of variable bool is ",typeof(bool))
+// console.log("The value of bool is  ",bool)
+
+let obj={
+    name:"Ahmed Sohail",
+    teacherName:"Hitesh Choudary",
+    channelName:"Chai aur code"
+}
+
+for(let key in obj){
+    // console.log(obj[key])
+}
+
+
+let array=["Ahmed", 21, true];
+for (let i=0;i<array.length;i++){
+    // console.log(array[i])
+}
+
+
+// Activity 04
+// task 5
+let resAssign="hello there";
+console.log(resAssign);
+resAssign="Hello World";
+console.log(resAssign);
+
+// Activity 05
+// task 06
+
+const value="Ahmed Sohail";
+console.log(value);
+
+let a= 123;
+value=a
+console.log(value)
+// gives type error assignment to const variable
+
