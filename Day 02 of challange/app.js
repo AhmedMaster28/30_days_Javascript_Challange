b3814ec869cbe0b87784d2674e9a35bd552506cd
AhmedMaster28 <ahmednsohail0@gmail.com>
2024-07-28 02:09:57 +0500

    challange number 2 completed

---
+++ Day 02 of challange/app.js
@@ -0,0 +1,111 @@
+//Activity 01 Arithmetic Operator
+//Task 01
+// let a = +prompt("Enter the first number");
+// let b = +prompt("Enter the second number");
+// let total = a + b;
+// let ans = document.querySelector("#answer");
+// ans.innerHTML=total;
+// console.log(total);
+
+// Task 02,03,04
+// let m1 = +prompt("Enter the first number");
+// let m2 = +prompt("Enter the second number");
+// let arithSymbol = prompt("Enter the operation you want");
+let Total;
+// // if (arithSymbol == "+") {
+//   Total = m1 + m2;
+// //   ans.innerHTML = Total;
+// //   console.log(Total);
+// }
+
+// // if (arithSymbol == "-") {
+//   Total = m2 - m1;
+// //   ans.innerHTML = Total;
+// //   console.log(Total);
+// }
+
+// // if (arithSymbol == "*") {
+//   Total = m1 * m2;
+// //   ans.innerHTML = Total;
+// //   console.log(Total);
+// }
+
+// // if (arithSymbol == "/") {
+//   Total = m2 / m1;
+// //   ans.innerHTML = Total;
+// //   console.log(Total);
+// }
+
+// Task 05
+// let remNum1=+prompt("Enter number one  ")
+// let remNum2=+prompt("Enter number second  ")
+// let answer=remNum1%remNum2
+// console.log(answer);
+
+// Activity 02 Assignment Operator
+// Task 06,07
+
+// let user=+prompt("Enter the number")
+// let plus=prompt("Plus or Minus")
+// if (plus =="Plus"){
+//     user +=1
+
+//     console.log(user)
+// }
+// else if(plus =="Minus"){
+//     user -=1
+//     console.log(user)
+// }
+// else{
+//     console.log("Wrong Input")}
+
+// let p = document.getElementById("p");
+// p.innerHTML = 0;
+// function count() {
+//   currentCounter = parseInt(p.innerHTML);
+//   p.innerHTML = currentCounter + 1;
+// }
+
+// Activity 03
+// Task 08, 09, 10
+// let com1=+prompt("Enter number one  ");
+// let com2=+prompt("Enter number second  ");
+// if (com1>com2){
+//     document.write("com1 is greater than com2")
+// }
+// else if(com1<com2){
+//     document.write("com2 is greater than com1")
+// }
+// else if(com1==com2){
+//     document.write("com1 is equal to com2")
+// }
+
+// let com3=3
+// let com4="3"
+
+// if (com3===com4){
+//     console.log("com3 and com4 are equal")
+// }
+// else{
+//     console.log("com3 and com4 are not equal")
+// }
+
+// Activity 04
+// Task 11, 12, 13
+// let op1 = 1;
+// let op2 = 2;
+// if (op1 == 2 && op2 == 1) {
+//   console.log("op1 and op2 are equal");
+// } else if (op1 == 1 || op2 == 2) {
+//   console.log("One of them are equal");
+// } else if (op1 != op2) {
+//   console.log("op1 and op2 are not equal");
+// } else {
+//   console.log("Wrong Input");
+// }
+
+// Activity 05
+// Task 14
+let age=+prompt("Enter a number");
+let a=age>0?"The given number is positive":"The given number is negative";
+alert(a);
